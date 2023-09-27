7dc195c0c7eec390db3b3cacea77918a470eda04
PH-Rocha <phr123silva@gmail.com>
2023-09-27 18:04:52 -0300

    js exercicios

---
+++ teste/javascript Teste.js
@@ -0,0 +1,39 @@
+/*Faça um script que pede duas notas de um aluno. Em seguida ele deve calcular a média do aluno e dar o seguinte resultado:
+
+    A mensagem "Aprovado", se a média alcançada for maior ou igual a sete;
+    A mensagem "Reprovado", se a média for menor do que sete;
+    A mensagem "Aprovado com Distinção", se a média for igual a dez.*/
+
+/*var nota1 = 10;
+var nota2 = 10;
+var nota3 = 10;
+var nota4 = 10;
+var media;
+
+media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+console.log(media);
+
+if(media == 10){
+    console.log("Aprovado com distinção")
+} else if(media >= 6){
+    console.log("Aprovado");
+} else {
+    console.log("Reprovado")
+}
+*/
+
+//Faça um script que leia três números inteiros e mostre o maior deles.
+
+/* var n1 = 68;
+var n2 = 9;
+var n3 = 109;
+
+if(n1 > n2 && n1 > n3){
+    console.log("Número 1 é o maior");
+} else if (n2 > n1 && n2 > n3){
+    console.log("Númeor 2 é o maior");
+} else if (n3 > n1 && n3 > n2){
+    console.log("Númeor 3 é o maior");
+}*/
+
