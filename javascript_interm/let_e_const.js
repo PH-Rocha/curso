4351c4913884e485ef6e857862bdd8b1d8039e93
PH-Rocha <phr123silva@gmail.com>
2023-10-24 15:53:58 -0300

    bug fixes

---
+++ javascript_interm/let_e_const.js
@@ -0,0 +1,32 @@
+//var idade = 18;
+//let idade = 18;
+
+/* VAR (escopo global) - escopo fora do bloco
+   LET (escopo local) - escopo restrito ao bloco
+   CONST (não permitindo reatribuição e nem redeclaração) - constante 
+   
+   var -
+        * A variável VAR é declarada e iniciada no escopo da função.
+        * Não respeitando bloco e permintindo a redeclaração e reatribuição.
+    
+   let -
+        * A variável LET é declarada no escopo da função, mas só é inicializada posteriormente
+        * Respeitando bloco e permitindo reatribuição, mas NÂO perminte a redeclaração.
+    
+   const - 
+        * A variável CONST é declarada no escopo da função, mas só é inicializada posteriormente
+        * Respeitando bloco e NÂO permitindo reatribuição NEM redeclaração
+   */
+/*function nome(){
+    var idade = 10;
+    console.log(idade);
+}
+
+nome();
+
+console.log(idade);*/
+
+
+// ARROW functions
+
+
