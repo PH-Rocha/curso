f41d10a96b1dbb897597425007ae8f798cce7efc
PH-Rocha <phr123silva@gmail.com>
2023-10-31 17:57:48 -0300

    estruturas de repetição

---
+++ ECMAScript/default_parameter.js
@@ -0,0 +1,19 @@
+/* sem default parameter
+function mutiply(a, b){
+    return a * b;
+}
+
+console.log(mutiply(5));
+console.log(mutiply(5, 2));
+*/
+
+/* com default parameter
+ 
+function multiply(a, b=1){
+    return a * b;
+}
+
+console.log((multiply(5)));
+console.log(multiply(5, 2));
+console.log(multiply(5, undefined));
+*/
